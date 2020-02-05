9dfe9b9dca14e6858430866f0b36a91152e59bbb
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-02-05 17:39:50 -0300

    Initial commit

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <span>Copyright <strong>Rafael</strong> 2020</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding-right: 10px;
+    }
+</style>
